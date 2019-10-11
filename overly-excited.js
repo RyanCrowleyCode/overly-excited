8b1f655cc912962d565e6277171f26e390a75eab
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-11 08:51:14 -0500

    Completed the Stacking Words exercise

--- overly-excited.js
+++ overly-excited.js
@@ -46,8 +46,13 @@ function addExcitement (theWordArray) {
 
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
+        buildMeUp += theWordArray[i] + " "
+        if (i === theWordArray.length -1) {
+            buildMeUp += "..."
+        }
 
         // Print buildMeUp to the console
+        console.log(buildMeUp)
     }
 
 }
