d5c8c0a8a1c23baac635cd76a8b7da0526e99000
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-11 09:00:33 -0500

    set up more-exciting to begin the exercise

--- overly-excited.js
+++ overly-excited.js
@@ -1,7 +1,10 @@
-// Practice: Stacking Words
-// The learning objects of this exercise is to use iteration and string concatenation
+// Practice: More Exciting
 
-// Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
+// The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one operation when a certain conditions is true, and a different operation if that condition is false.
+
+// Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
+
+// If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
 
 // Example output:
 
@@ -9,47 +12,46 @@
 
 // The walrus
 
-// The walrus danced
+// The walrus danced!
 
-// The walrus danced through
+// The walrus danced! through
 
-// The walrus danced through the
+// The walrus danced! through the
 
-// The walrus danced through the trees
+// The walrus danced! through the trees!
 
-// The walrus danced through the trees in
+// The walrus danced! through the trees! in
 
-// The walrus danced through the trees in the
+// The walrus danced! through the trees! in the
 
-// The walrus danced through the trees in the light
+// The walrus danced! through the trees! in the light!
 
-// The walrus danced through the trees in the light of
+// The walrus danced! through the trees! in the light! of
 
-// The walrus danced through the trees in the light of the
+// The walrus danced! through the trees! in the light! of the
 
-// The walrus danced through the trees in the light of the moon ...
-
-// Paste the following code into overly-excited.js
+// The walrus danced! through the trees! in the light! of the moon!
 
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray) {
+
+
+function addExcitement(theWordArray) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp += theWordArray[i] + " "
-        if (i === theWordArray.length -1) {
-            buildMeUp += "..."
-        }
+        /*
+         If the current word's place (not index) in the array
+         is evenly divisible by 3, add an exclamation point
+         to the end of the word and then concatenate it to
+         `buildMeUp`.
+
+         Otherwise, just concatenate the word itself.
+      */
+
 
         // Print buildMeUp to the console
         console.log(buildMeUp)
